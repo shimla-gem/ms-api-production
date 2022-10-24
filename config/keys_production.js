0f782ebb021707009c7706945a73175142a5b6e6
shimla-gem <116479281+shimla-gem@users.noreply.github.com>
2022-10-24 15:24:27 +0530

    2nd commit

--- config/keys_production.js
+++ config/keys_production.js
@@ -1,8 +1,8 @@
-// module.exports = {
-//     mongoURI: process.env.MONGO_URI
-//     ,secretOrKey:process.env.SECRECT_OR_KEY
-// };
 module.exports = {
-    mongoURI: 'mongodb+srv://jafranjemal:jafran123@cluster0.yai9p.mongodb.net/shimla-gem?retryWrites=true&w=majority'
-    ,secretOrKey:'secrect'
+    mongoURI: process.env.MONGO_URI
+    ,secretOrKey:process.env.SECRECT_OR_KEY
 };
+// module.exports = {
+//     mongoURI: 'mongodb+srv://shimla-gem:shimlateam1@cluster0.wsyph7s.mongodb.net/shimla-gem-production?retryWrites=true&w=majority' //'mongodb+srv://jafranjemal:jafran123@cluster0.yai9p.mongodb.net/shimla-gem?retryWrites=true&w=majority'
+//     ,secretOrKey:'secrect'
+// };
